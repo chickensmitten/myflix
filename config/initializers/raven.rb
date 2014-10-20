1f07142600655dfe29e83a90d802f81e5ce03992
Sheng <chia.shengyeong@gmail.com>
2014-10-20 15:19:19 +0800

    added deploy.rake

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,5 @@
 require 'raven'
+require 'raven/sidekiq'
 
 Raven.configure do |config|
   config.dsn = 'https://6d0f1a17cf4b4fddac25eef4bdcc0031:ce9c18d5dfac425fb27e6d97866f7997@app.getsentry.com/31821'
